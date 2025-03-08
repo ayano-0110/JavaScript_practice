2993080f3fb931d72864c6e2e0a8cf2742c0d1e6
ayano-0110 <gd.clbom@gmail.com>
2025-03-08 15:59:03 +0000

    課題を提出

---
+++ kadai/kadai07.js
@@ -0,0 +1,14 @@
+function area(p1, p2) {
+    let p = p1 * p2 / 2;        
+    return p;               
+}
+
+console.log(area(10, 20));
+
+
+var area = function(p1, p2) {
+    let p = p1 * p2 / 2;       
+    return p;               
+}
+
+console.log(area(10, 10));
